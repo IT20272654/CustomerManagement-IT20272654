1fbb5f11831f9e385fd1851a01f1cc18f2ed99bb
IT20272654 <shavindurajapaksha@gmail.com>
2022-05-15 21:58:50 +0530

    MySQL Database file

--- DataBase/customer.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM electrogrid.customer
-LIMIT 0, 1000
-
--- Date: 2022-05-15 15:53
-*/
-INSERT INTO `` (`customerID`,`customerCode`,`customerName`,`customerPhone`,`customerDesc`) VALUES (1,'101e','Nihal','0776546532','Paid');
-INSERT INTO `` (`customerID`,`customerCode`,`customerName`,`customerPhone`,`customerDesc`) VALUES (3,'103f','Kamal','0873234235','NotPaid');
-INSERT INTO `` (`customerID`,`customerCode`,`customerName`,`customerPhone`,`customerDesc`) VALUES (5,'105g','Vinal','0763432532','Paid');
